2d991f187c7b0b8e4120ad7f3e971d0e715dbd2d
MateuszGurdala <mateusz.gurdala.mg@gmail.com>
2023-09-17 20:12:10 +0200

    Moved the kernel data retrieval initiative to the server

---
+++ include/monitor/get_mbuffer_q.h
@@ -0,0 +1,17 @@
+#ifndef _GET_MBUFFER_Q_H
+#define _GET_MBUFFER_Q_H
+
+#include <phoenix/monitor.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int get_mbuffer_q(m_buffer *mbuffer_qcpy);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
---
+++ include/monitor/get_mdata_q.h
@@ -0,0 +1,17 @@
+#ifndef _GET_MDATA_Q_H
+#define _GET_MDATA_Q_H
+
+#include <phoenix/monitor.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int get_mdata_q(m_data *mdata_qcpy);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
