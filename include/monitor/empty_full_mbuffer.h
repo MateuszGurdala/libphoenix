d5cc78e700d3540b106c382e25fd506d3472e9c9
MateuszGurdala <mateusz.gurdala.mg@gmail.com>
2023-09-22 22:00:18 +0200

    Fixed on-demand data

---
+++ include/monitor/empty_full_mbuffer.h
@@ -0,0 +1,17 @@
+#ifndef _EMPTY_FULL_MBUFFER_H
+#define _EMPTY_FULL_MBUFFER_H
+
+#include <phoenix/monitor.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int empty_full_mbuffer(unsigned ebuff, m_data* buff_cpy);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
